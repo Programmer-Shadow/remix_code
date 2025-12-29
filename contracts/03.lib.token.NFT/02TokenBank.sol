8e4efb6a0d927912dd4b7d97e53f311a017a5466
Programmer-Shadow <519686353@qq.com>
2025-12-29 12:06:35 +0800

    完善TokenBank

--- contracts/03.lib.token.NFT/02TokenBank.sol
+++ contracts/03.lib.token.NFT/02TokenBank.sol
@@ -7,10 +7,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract TokenBank {
     // IERC20的使用
     IERC20 public token;
-    //event的使用
-    event Deposit(address indexed from,address indexed to, uint256 amount);
-    event Withdraw(address indexed from,address indexed to, uint256 amount);
-
+    //event的使用 一般不写第二个参数，to就是到自己这个地址，显而易见
+    //event Deposit(address indexed from,address indexed to, uint256 amount);
+    //event Withdraw(address indexed from,address indexed to, uint256 amount);
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
     mapping (address=>uint256) balances;
     constructor(address tokenAddress){
         token = IERC20(tokenAddress);//构造时写法
@@ -20,9 +21,11 @@ contract TokenBank {
     // 存入 Token
     function deposit(uint256 amount) external {
         require(amount > 0,"amount must bigger than 0");
-        token.transferFrom(msg.sender, address(this), amount);
+        require(amount <= token.balanceOf(msg.sender),"sender insufficient balance");
+        bool success = token.transferFrom(msg.sender, address(this), amount);
+        require(success,"transfer failed");
         balances[msg.sender]+=amount;
-        emit Deposit(msg.sender, address(this), amount);
+        emit Deposit(msg.sender, amount);
     }
     
     // 取出 Token
@@ -30,8 +33,9 @@ contract TokenBank {
         require(amount>0,"amount  must bigger  than  0");
         require(amount<=balances[msg.sender],"insufficient balance");
         balances[msg.sender]-=amount;
-        token.transfer(msg.sender, amount);
-        emit  Withdraw(address(this), msg.sender, amount);
+        bool success = token.transfer(msg.sender, amount);
+        require(success,"transfer failed");
+        emit  Withdraw(msg.sender, amount);
     }
     function getBalance(address user) view public returns(uint){
         return balances[user];
